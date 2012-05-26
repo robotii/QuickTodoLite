c8a0a26c0fc09cfc4279c6f4fa742152ccdcf335
Peter Arthur <pete.arthur@gmail.com>
2012-05-26 16:58:58 +0100

    Updated licence on all source files

--- src/org/jetpad/quicktodofree/TodoEditor.java
+++ src/org/jetpad/quicktodofree/TodoEditor.java
@@ -1,5 +1,7 @@
-/*
- * Copyright (C) 2009 Peter Arthur
+/**
+ * ------------------------------------------------------------
+ *                       QuickTodo Lite
+ * ------------------------------------------------------------
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
